62f88d0c4ef1f974ae411031f6c6c0e3ccfe4637
blowa <light06120602@gmail.com>
2026-02-03 22:31:16 +0900

    build.gradle.kts の更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "NyankoWars"
-include(":app")
