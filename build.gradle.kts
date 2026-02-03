1ee9b6b08848091b2789ef5c8301762b87feb36b
blowa <light06120602@gmail.com>
2026-02-03 22:30:15 +0900

    build.gradle.kts の更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,17 @@
-plugins {
-    id("com.android.application") version "8.1.2"
-    id("org.jetbrains.kotlin.android") version "1.9.10"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
-
-android {
-    namespace = "com.nyankowars"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.nyankowars"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-}
+rootProject.name = "NyankoWars"
+include(":app")
