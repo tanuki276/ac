8242ce436cea937eabe92dbf006aeae6d52a9da0
blowa <light06120602@gmail.com>
2026-02-03 16:48:10 +0900

    MainActivity.kt の作成

---
+++ app/src/main/java/com/nyankowars/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.nyankowars
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.nyankowars.presentation.screens.MainScreen
+import com.nyankowars.presentation.theme.NyankoWarsTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setContent {
+            NyankoWarsTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
