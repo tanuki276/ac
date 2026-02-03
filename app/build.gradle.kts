461874149b207ad402bed9254367718837390163
blowa <light06120602@gmail.com>
2026-02-03 18:30:41 +0900

    build.gradle.kts の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,35 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+cat <<EOF > app/build.gradle.kts
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    // settingsで定義したバージョンが自動適用されます
+}
+
+android {
+    namespace = "com.nyankowars"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.nyankowars"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven(url = "https://jitpack.io")
-    }
+dependencies {
+    // settingsで定義したカタログ(libs)を使う
+    implementation(libs.findLibrary("androidx-core").get())
+    implementation(libs.findLibrary("androidx-lifecycle").get())
 }
-
-rootProject.name = "NyankoWars"
-include(":app")
+EOF
