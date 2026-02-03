023d11e7ffce88c1a128653c2f20af58791fdfbe
blowa <light06120602@gmail.com>
2026-02-03 18:53:44 +0900

    build.gradle.kts の作成

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application") version "8.1.2"
+    id("org.jetbrains.kotlin.android") version "1.9.10"
+}
+
+android {
+    namespace = "com.nyankowars"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.nyankowars"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+}
